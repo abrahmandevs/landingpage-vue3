e56bb94c64f451194cf137093c3857b56b9970db
arrahmna-dev <dra390@gmail.com>
2023-10-24 00:46:41 +0600

    modify header dialog mobile menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -56,38 +56,6 @@ t
 		<nav class="mx-auto flex items-center justify-between" aria-label="Global">
 			<div class="grow flex items-center">
 				<a href="/" class="-m-1.5 p-1.5 text-xl font-semibold text-black hover:text-black mr-4">LoGo</a>
-				<!-- <Popover class="relative">
-					<PopoverButton class="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-						Product
-						<ChevronDownIcon class="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-					</PopoverButton>
-
-					<transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
-						<PopoverPanel class="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-							<div class="p-4">
-								<div v-for="item in products" :key="item.name" class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
-									<div class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-										<component :is="item.icon" class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
-									</div>
-									<div class="flex-auto">
-										<a :href="item.href" class="block font-semibold text-gray-900">
-											{{ item.name }}
-											<span class="absolute inset-0" />
-										</a>
-										<p class="mt-1 text-gray-600">{{ item.description }}</p>
-									</div>
-								</div>
-							</div>
-							<div class="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-								<a v-for="item in callsToAction" :key="item.name" :href="item.href" class="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100">
-									<component :is="item.icon" class="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-									{{ item.name }}
-								</a>
-							</div>
-						</PopoverPanel>
-					</transition>
-				</Popover> -->
-				<!-- dropdown -->
 				<PopoverGroup class="hidden md:block">
 					<!-- first loop  -->
 					<template v-for="(catetory, index) in data['categorys']">
@@ -170,21 +138,21 @@ t
 		</nav>
 
 		<TransitionRoot as="template" :show="mobileMenuOpen">
-			<Dialog as="div" class="relative z-40 lg:hidden bg-blue-500" @close="mobileMenuOpen = false">
+			<Dialog as="div" class="relative z-50 lg:hidden bg-blue-500" @close="mobileMenuOpen = false">
 				<TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100" leave-to="opacity-0">
 					<div class="fixed inset-0 bg-black bg-opacity-25" />
 				</TransitionChild>
 
 				<div class="bg-green-40 fixed inset-0 z-40 flex justify-end">
 					<TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700" enter-from="translate-x-full" enter-to="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-x-0" leave-to="translate-x-full">
-						<DialogPanel class="w-full sm:w-3/6 relative overflow-y-auto bg-white p-4 shadow-xl">
-							<div class="flex items-center justify-between p-4">
+						<DialogPanel class="w-5/6 sm:w-3/6 relative overflow-y-auto bg-white border shadow-xl">
+							<div class="sticky flex items-center justify-between p-5">
 								<a href="/" class="-m-1.5 p-1.5 text-xl font-semibold text-black hover:text-black mr-4">LoGo</a>
 								<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
 									<XMarkIcon class="h-6 w-6" aria-hidden="true" />
 								</button>
 							</div>
-							<div class="mt-6">
+							<div class="p-1.5">
 								<div class="divide-y divide-gray-500/10">
 									<div class="mb-3 space-y-1">
 										<!-- first loop -->
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    // base: '/' not working on github action page
-    // base: '/' not working on github action page
     base: '/landingpage-vue3'
 })
