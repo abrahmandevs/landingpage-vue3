5ef096a96cd20299aef58c5cf7423cfb507b38b2
arrahmna-dev <dra390@gmail.com>
2023-10-23 23:36:24 +0600

    build

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [vue()],
     // base: '/' not working on github action page
+    // base: '/' not working on github action page
     base: '/landingpage-vue3'
 })
